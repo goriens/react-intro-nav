8a457f1414caac2ca1b74040bd026b1586603969
Sohaib gour <76823175+goriens@users.noreply.github.com>
2022-04-09 15:37:08 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.png";
+import "./App.css";
 
 function App() {
+  const navArr = ["Services", "Projects", "About"];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="nav">
+        <div className="box1">
+          <img src={logo} className="logo" alt="logo" />
+        </div>
+        <div className="box2">
+          <ul>
+            {navArr.map((e) => {
+              return <NavMenu menu={e} />;
+            })}
+          </ul>
+        </div>
+        <div className="box3">
+          <button>Contact</button>
+        </div>
+      </nav>
     </div>
   );
+}
+function NavMenu(props) {
+  return <li>{props.menu}</li>;
 }
 
 export default App;
